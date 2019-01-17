4ede8238e4886a4903de757efeff6b07c9ce4dca
Jie Lu <jie.lu@ge.com>
2019-01-16 23:23:37 -0500

    jlu 1/16/19: updated comments, function and parameter names so the intention and results are more clear to developers.
    Signed-off-by: Jie Lu <jie.lu@ge.com>

--- parseSchema/index.js
+++ parseSchema/index.js
@@ -2,12 +2,17 @@
 
 const Parse = require('parse/node');
 
-function updateSchemaOnlyWhenNecessary(existingSchemaData, intendedSchema)
+/**
+ * This function compares the fields in schemaFieldsToAdd against the existing schema, excludes any existing fields from the former,
+ * and sends an update request to Parse server to add new fields to the schema.
+ * Note: this function does not delete any fields.
+ */
+function addAnyNewFieldsToSchema(existingSchemaData, schemaFieldsToAdd)
 {
   // remove any existing fields from the request.
-  Object.getOwnPropertyNames(existingSchemaData.fields).forEach(existingField => { delete intendedSchema._fields[existingField] ; });
-  return (Object.getOwnPropertyNames(intendedSchema._fields).length + Object.getOwnPropertyNames(intendedSchema._indexes).length > 0 ) ? 
-    intendedSchema.update() : Promise.resolve(existingSchemaData);
+  Object.getOwnPropertyNames(existingSchemaData.fields).forEach(existingField => { delete schemaFieldsToAdd._fields[existingField] ; });
+  return (Object.getOwnPropertyNames(schemaFieldsToAdd._fields).length + Object.getOwnPropertyNames(schemaFieldsToAdd._indexes).length > 0 ) ? 
+    schemaFieldsToAdd.update() : Promise.resolve(existingSchemaData);
 }
 
 function setParseSchema (serverConfig) {
@@ -40,9 +45,9 @@ function setParseSchema (serverConfig) {
     .addBoolean('isInCareForHepC')
     .addBoolean('isInCareForHiv')
 
+    .addDate('ageOfFirstInjection')
     .addDate('dateOfBirth')
     .addDate('dateOfLastVisit')
-    .addDate('ageOfFirstInjection')
 
     .addNumber('syringesGivenAggregate')
     .addNumber('syringesTakenAggregate')
@@ -60,8 +65,8 @@ function setParseSchema (serverConfig) {
     .addString('zipCode')
 
     .get()
-    .then(existingSchemaData => updateSchemaOnlyWhenNecessary(existingSchemaData, contactSchema))
-    .catch(error => ((error.code === Parse.Error.INVALID_CLASS_NAME) ? contactSchema.save() : Promise.reject(error)))
+    .then(existingSchemaData => addAnyNewFieldsToSchema(existingSchemaData, contactSchema))
+    .catch(error => ((error.code === Parse.Error.INVALID_CLASS_NAME) ? contactSchema.save() : Promise.reject(error)));
 
   const eventSchema = new Parse.Schema('event');
 
@@ -77,8 +82,8 @@ function setParseSchema (serverConfig) {
     .addBoolean('isInCareForHepC')
     .addBoolean('isInCareForHiv')
     .addBoolean('isOutreach')
-    .addBoolean('narcanWasTaken')
     .addBoolean('narcanWasOffered')
+    .addBoolean('narcanWasTaken')
 
     .addDate('date')
     .addDate('dateOfBirth')
@@ -104,8 +109,8 @@ function setParseSchema (serverConfig) {
     .addString('zipCode')
 
     .get()
-    .then(existingSchemaData => updateSchemaOnlyWhenNecessary(existingSchemaData, eventSchema))
-    .catch(error => ((error.code === Parse.Error.INVALID_CLASS_NAME) ? eventSchema.save() : Promise.reject(error)))
+    .then(existingSchemaData => addAnyNewFieldsToSchema(existingSchemaData, eventSchema))
+    .catch(error => ((error.code === Parse.Error.INVALID_CLASS_NAME) ? eventSchema.save() : Promise.reject(error)));
 }
 
 module.exports = setParseSchema;
